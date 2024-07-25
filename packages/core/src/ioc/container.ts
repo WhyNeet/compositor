3a8c99546378ce8df991a604c162d84e36b41274
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-25 22:18:15 +0300

    feat(core): add bean registration methods to container class

--- packages/core/src/ioc/container.ts
+++ packages/core/src/ioc/container.ts
@@ -1,1 +1,22 @@
-export class Container {}
+import { BeanDefinition } from "./bean/bean-definition";
+import { BeanDefinitionRegistry } from "./bean/bean-definition-registry";
+import { InjectionToken } from "./injection-token";
+import { Ctor } from "./types";
+
+export class Container {
+  private _beanDefinitionRegistry: BeanDefinitionRegistry;
+
+  constructor() {
+    this._beanDefinitionRegistry = new BeanDefinitionRegistry();
+  }
+
+  public registerCtor(token: InjectionToken, ctor: Ctor) {
+    const definition = BeanDefinition.class(token, ctor);
+    this._beanDefinitionRegistry.put(definition);
+  }
+
+  public registerFactory(token: InjectionToken, factory: () => unknown) {
+    const definition = BeanDefinition.factory(token, factory);
+    this._beanDefinitionRegistry.put(definition);
+  }
+}
--- packages/core/src/ioc/error/no-bean-annotation.ts
+++
@@ -1,7 +0,0 @@
-import { Ctor } from "../types";
-
-export class NoBeanAnnotationError extends Error {
-  constructor(ctor: Ctor) {
-    super(`No @Bean annotation found on class "${ctor.name}"`);
-  }
-}
