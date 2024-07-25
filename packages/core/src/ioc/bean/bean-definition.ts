5f01d63ef631b78002c36ac9de3bef7588176848
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-25 21:27:21 +0300

    feat(core): add factory to bean definition

--- packages/core/src/ioc/bean/bean-definition.ts
+++ packages/core/src/ioc/bean/bean-definition.ts
@@ -6,39 +6,78 @@ import { Scope } from "./bean-scope";
 
 export class BeanDefinition {
   private _token: InjectionToken;
-  private _class: Ctor;
+  private _class: Ctor | null;
+  private _factory: () => unknown;
   private _dependencies: InjectionToken[];
   private _lazy: boolean;
   private _scope: Scope;
 
-  constructor(token: InjectionToken, ctor: Ctor) {
-    this._token = token;
-    this._class = ctor;
+  public static class(token: InjectionToken, ctor: Ctor) {
+    const definition = new BeanDefinition();
 
-    this.readMetadata();
+    definition._token = token;
+    definition._class = ctor;
+
+    definition.readMetadata(definition._class);
+
+    return definition;
   }
 
-  private readMetadata() {
-    // const injectable: boolean = Reflect.getOwnMetadata(METADATA_KEY.IOC_INJECTABLE, this._class);
-    // if (typeof injectable !== 'boolean') throw new NoBeanAnnotationError(this._class);
+  public static factory(token: InjectionToken, factory: () => unknown) {
+    const definition = new BeanDefinition();
 
+    definition._token = token;
+    definition._factory = factory;
+
+    return definition;
+  }
+
+  private readMetadata(obj: unknown) {
     const dependencies: InjectionToken[] = Reflect.getOwnMetadata(
       METADATA_KEY.IOC_DEPENDENCIES,
-      this._class,
+      obj,
     );
     this._dependencies = dependencies;
 
     const lazy: boolean =
-      Reflect.getOwnMetadata(METADATA_KEY.IOC_LAZY, this._class) ?? false;
+      Reflect.getOwnMetadata(METADATA_KEY.IOC_LAZY, obj) ?? false;
     this._lazy = lazy;
 
     const scope: Scope =
-      Reflect.getOwnMetadata(METADATA_KEY.IOC_SCOPE, this._class) ??
-      Scope.Singleton;
+      Reflect.getOwnMetadata(METADATA_KEY.IOC_SCOPE, obj) ?? Scope.Singleton;
     this._scope = scope;
   }
 
   public getToken() {
     return this._token;
+  }
+
+  public isLazy() {
+    return this._lazy;
+  }
+
+  public getFactory() {
+    return this._factory;
+  }
+
+  public getFactoryResult() {
+    const result = this._factory();
+
+    // read metadata of class instance
+    this.readMetadata(result);
+
+    return result;
+  }
+
+  public getClass() {
+    return this._class;
+  }
+
+  public getScope() {
+    return this._scope;
+  }
+
+  public getDependencies() {
+    return this._dependencies ?? [];
   }
 }
