b45bb6afe08a10bc6c4db8816a5eb85b2544cff2
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-28 14:57:34 +0300

    chore(core): add exports

---
+++ packages/core/src/ioc/bean/index.ts
@@ -0,0 +1,5 @@
+export * from "./bean";
+export * from "./bean-definition";
+export * from "./bean-definition-registry";
+export * from "./bean-scope";
+export * from "./singleton-bean-registry";
--- packages/core/src/ioc/index.ts
+++ packages/core/src/ioc/index.ts
@@ -1,1 +1,5 @@
 export * from "./container";
+export * from "./bean";
+export * from "./constants";
+export * from "./injection-token";
+export * from "./types";
