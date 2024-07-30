ab5da46d0a4f848210d67c7a93a3ca5ecf583487
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-07-30 22:41:54 +0300

    feat(core): add optional qualifier to autowired

--- packages/core/src/ioc/decorator/autowired.ts
+++ packages/core/src/ioc/decorator/autowired.ts
@@ -2,22 +2,28 @@ import { METADATA_KEY } from "../constants";
 import { InjectionToken } from "../injection-token";
 import { Ctor, FieldArg } from "../types";
 
-// biome-ignore lint/suspicious/noExplicitAny: allow any target
-export function Autowired(target: any, propertyKey: string) {
-  const propertyType: Ctor = Reflect.getOwnMetadata(
-    METADATA_KEY.DESIGN_TYPE,
-    target,
-    propertyKey,
-  );
-  const propertyToken: InjectionToken = propertyType.name;
+export function Autowired(qualifier?: InjectionToken) {
+  // biome-ignore lint/complexity/useArrowFunction: not using arrow function
+  // biome-ignore lint/suspicious/noExplicitAny: allow any target
+  return function (target: any, propertyKey: string) {
+    const propertyType: Ctor = Reflect.getOwnMetadata(
+      METADATA_KEY.DESIGN_TYPE,
+      target,
+      propertyKey,
+    );
+    const propertyToken: InjectionToken = propertyType.name;
 
-  const fieldDependencies: FieldArg[] =
-    Reflect.getOwnMetadata(METADATA_KEY.IOC_FIELD_ARGS, target.constructor) ??
-    [];
-  fieldDependencies.push({ token: propertyToken, property: propertyKey });
-  Reflect.defineMetadata(
-    METADATA_KEY.IOC_FIELD_ARGS,
-    fieldDependencies,
-    target.constructor,
-  );
+    const fieldDependencies: FieldArg[] =
+      Reflect.getOwnMetadata(METADATA_KEY.IOC_FIELD_ARGS, target.constructor) ??
+      [];
+    fieldDependencies.push({
+      token: qualifier ?? propertyToken,
+      property: propertyKey,
+    });
+    Reflect.defineMetadata(
+      METADATA_KEY.IOC_FIELD_ARGS,
+      fieldDependencies,
+      target.constructor,
+    );
+  };
 }
