2e2ceb697c81e0a4e1a030a4935409914056439f
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-08-07 12:15:05 +0300

    feat(core): add middleware method decorator

--- packages/core/src/application/constants/metadata-keys.ts
+++ packages/core/src/application/constants/metadata-keys.ts
@@ -3,3 +3,5 @@ export const CONTROLLER_HANDLER = "application:controller:handler";
 export const CONTROLLER_HANDLERS = "application:controller:handlers";
 
 export const HANDLER_PROVIDE = "application:controller:handler:provide";
+export const HANDLER_MIDDLEWARE = (key: string | symbol) =>
+  `application:controller:handler:${String(key)}`;
---
+++ packages/core/src/application/decorator/middleware.ts
@@ -0,0 +1,21 @@
+import { METADATA_KEY } from "../constants";
+import { Middleware as AbstractMiddleware } from "../features";
+
+export function Middleware(middleware: {
+  // biome-ignore lint/suspicious/noExplicitAny: any middleware constructor
+  new (...args: any[]): AbstractMiddleware;
+}) {
+  return function (
+    // biome-ignore lint/complexity/useArrowFunction: not using arrow function
+    // biome-ignore lint/suspicious/noExplicitAny: any target
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    const key = METADATA_KEY.HANDLER_MIDDLEWARE(propertyKey);
+    const middlewares: AbstractMiddleware[] =
+      Reflect.getOwnMetadata(key, target.constructor) ?? [];
+    middlewares.push(middleware);
+    Reflect.defineMetadata(key, middlewares, target.constructor);
+  };
+}
