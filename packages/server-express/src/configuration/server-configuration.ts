45f002aa52247ab61bac6c6a09602ae584917de6
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-08-08 15:44:10 +0300

    feat(server-express): modify configuration

--- packages/server-express/src/configuration/server-configuration.ts
+++ packages/server-express/src/configuration/server-configuration.ts
@@ -1,9 +1,11 @@
 import { Configuration, ConfigurationContext } from "@compositor/core";
+import { HandlerRegistrationAspect } from "../aspect";
 import { METADATA_KEY } from "../constants";
 import { ServerBean } from "../server";
 
 export class ServerConfiguration extends Configuration {
   configure(cx: ConfigurationContext): void {
     cx.registerCtor(METADATA_KEY.SERVER, ServerBean);
+    cx.registerCtor(HandlerRegistrationAspect);
   }
 }
