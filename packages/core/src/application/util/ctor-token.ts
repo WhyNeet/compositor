921986eea9683d93bd0571b74bcc102f4b56a487
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-08-09 16:00:04 +0300

    refactor(core): use ioc metadata inside ctor token retrieval utility

--- packages/core/src/application/util/ctor-token.ts
+++ packages/core/src/application/util/ctor-token.ts
@@ -1,4 +1,4 @@
 import { Ctor, METADATA_KEY } from "../../ioc";
 
 export const getCtorToken = <T extends Ctor>(ctor: T) =>
-  Reflect.getOwnMetadata(METADATA_KEY.DESIGN_TYPE, ctor);
+  Reflect.getOwnMetadata(METADATA_KEY.IOC_DEFAULT_TOKEN, ctor);
