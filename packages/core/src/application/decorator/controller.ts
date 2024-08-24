8f31eaf44a83de894b26451dddb6e8d04588bbf4
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-08-24 22:14:02 +0300

    feat(core): switch controller from decorator function to class

--- packages/core/src/application/decorator/controller.ts
+++ packages/core/src/application/decorator/controller.ts
@@ -1,14 +1,19 @@
-import { Bean } from "../../ioc";
+import { Bean, Ctor } from "../../ioc";
+import { Apply, ClassDecorator } from "../../shared";
 import { METADATA_KEY } from "../constants";
 import { HandlerPath } from "../features";
 
-export function Controller(...path: HandlerPath) {
-  return function (
-    // biome-ignore lint/complexity/useArrowFunction: not using arrow function
-    // biome-ignore lint/suspicious/noExplicitAny: any target
-    target: any,
-  ) {
+export class ControllerDecorator extends ClassDecorator<[HandlerPath]> {
+  apply<T extends Ctor>(target: T): void {
     Bean()(target);
-    Reflect.defineMetadata(METADATA_KEY.APPLICATION_CONTROLLER, path, target);
-  };
+    Reflect.defineMetadata(
+      METADATA_KEY.APPLICATION_CONTROLLER,
+      this.args()[0],
+      target,
+    );
+  }
 }
+
+export const Controller = (...path: HandlerPath) =>
+  Apply(ControllerDecorator, path);
+Controller.decorator = ControllerDecorator;
