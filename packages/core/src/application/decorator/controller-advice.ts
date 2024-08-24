662fb64f9e6db1222dfe330fa9278c35f4a4d25a
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-08-24 22:17:41 +0300

    feat(core): add controller advice decorator

---
+++ packages/core/src/application/decorator/controller-advice.ts
@@ -0,0 +1,12 @@
+import { Ctor } from "../../ioc";
+import { Apply, ClassDecorator } from "../../shared";
+import { Advice } from "./advice";
+import { Controller } from "./controller";
+
+export class ControllerAdviceDecorator extends ClassDecorator<[]> {
+  apply<T extends Ctor>(target: T): void {
+    Advice(Controller.decorator)(target);
+  }
+}
+
+export const ControllerAdvice = () => Apply(ControllerAdviceDecorator);
